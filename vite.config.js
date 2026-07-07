1baea289c17deefda5c3ca12470667de77109afa
works-ac <works.sahitya@gmail.com>
2026-07-07 15:24:02 +0530

    feat: added seo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
+
+const myAppRoutes = ["/notices", "/account", "/register", "/recover/account"];
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: "https://jnotifier.devapps.codingworks.in",
+      readable: true,
+      dynamicRoutes: myAppRoutes,
+    }),
+  ],
+});
