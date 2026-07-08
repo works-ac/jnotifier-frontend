6ae87c11456c3ddbef53610d1fe198c538a62ea7
works-ac <works.sahitya@gmail.com>
2026-07-08 15:19:45 +0530

    fix: minor changes

--- src/components/core/PdfOpenerDialog.jsx
+++ src/components/core/PdfOpenerDialog.jsx
@@ -57,7 +57,7 @@ function PdfOpenerDialog({
         <PdfViewer
           file={pdfBlob}
           disableDownloadable={!isDownloadable}
-          fileName={title}
+          fileName={`${title?.split(" ")?.join("-")?.toLowerCase()}.pdf`}
         />
       </DialogContent>
     </Dialog>
